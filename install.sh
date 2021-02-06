97d3200af85b180c804a5c27f283f18767e9f01c
Gyan Prakash Karn <karngyan@gmail.com>
2021-02-07 00:26:53 +0530

    shifting sudo db setups to install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,12 @@ wget https://dl.google.com/go/go1.15.7.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.15.7.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
-
 # database installation
 
 sudo apt update
 sudo apt install -y postgresql
+
+# db setup
+sudo -u postgres psql -c "CREATE ROLE xmeme LOGIN ENCRYPTED PASSWORD 'xmeme';"
+sudo -u postgres psql -c "CREATE DATABASE xmeme --owner xmeme"
 
--- server_run.sh
+++ server_run.sh
@@ -2,11 +2,6 @@
 
 cd backend
 
-# db setup
-sudo -u postgres psql -c "CREATE ROLE xmeme LOGIN ENCRYPTED PASSWORD 'xmeme';"
-sudo -u postgres psql -c "CREATE DATABASE xmeme --owner xmeme"
-
-
 # run server
 go build main.go
 chmod +x main
